2119bbc42d0b600887f5548a43ee591f57914b5c
Tolby <tolby.lam@gmail.com>
2022-06-07 01:42:55 -0700

    Read and parse user input

--- ASMT-02/src/main/Main.java
+++ ASMT-02/src/main/Main.java
@@ -1,9 +1,27 @@
-package edu.sfsu;
+package main;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        //split into words and turn them into lowercase
+        String[] words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
+
+        //parse the input
+        InputParser inputParser = new InputParser();
+        ParsedQuery parsedQuery = InputParser.parseInput(words);
+
+        //print the result
+        System.out.println(parsedQuery.getWord());
+        System.out.println(parsedQuery.getPartOfSpeech());
+        System.out.println(parsedQuery.getIsDistinct());
+        System.out.println(parsedQuery.getIsReversed());
     }
 }
